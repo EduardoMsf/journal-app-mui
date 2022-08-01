9a3616ae8405b2e7eb855d8616a8a0d1a56c25f0
Eduardo Samaniego <samaniegomsf@gmail.com>
2022-08-01 01:55:48 -0500

    Creamos la vista nothing selected

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,12 @@
 import { Typography } from "@mui/material"
 import { JournalLayout } from "../Layout/JournalLayout"
+import { NothingSelectedView } from "../views/NothingSelectedView"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>Texto text mucho texto</Typography>
+      {/* <Typography>Texto text mucho texto</Typography> */}
+      <NothingSelectedView />
     </JournalLayout>
   )
 }
