eeecd44d4df7ae1fddf82ded2f9f1fefc9eb90d2
Eduardo Samaniego <samaniegomsf@gmail.com>
2022-07-31 23:33:48 -0500

    Creamos el sidebar

--- src/journal/Layout/JournalLayout.jsx
+++ src/journal/Layout/JournalLayout.jsx
@@ -1,5 +1,6 @@
+import { Toolbar } from "@mui/material"
 import { Box } from "@mui/system"
-import { NavBar } from "../components"
+import { NavBar, SideBar } from "../components"
 
 const drawerWidth = 240
 
@@ -7,7 +8,9 @@ export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display:'flex' }}>
       <NavBar  drawerWidth={drawerWidth} />
+      <SideBar drawerWidth={drawerWidth}/>
       <Box component='main' sx={{flexGrow:1, p:3}}>
+        <Toolbar />
         { children }
       </Box>
 
