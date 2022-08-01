0015748fbd71b49df5b50ec45343f7eebf8a669d
Eduardo Samaniego <samaniegomsf@gmail.com>
2022-08-01 02:08:03 -0500

    Creamos el Note view

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,14 @@
 import { Typography } from "@mui/material"
 import { JournalLayout } from "../Layout/JournalLayout"
+import { NoteView } from "../views/NoteView"
 import { NothingSelectedView } from "../views/NothingSelectedView"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
       {/* <Typography>Texto text mucho texto</Typography> */}
-      <NothingSelectedView />
+      {/* <NothingSelectedView /> */}
+      <NoteView />
     </JournalLayout>
   )
 }
