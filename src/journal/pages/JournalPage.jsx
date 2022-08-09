2ee8e5bcd1c1cccaba441ff9ab66889365f7d3d5
Eduardo Samaniego <samaniegomsf@gmail.com>
2022-08-08 23:14:47 -0500

    Creamos las notas en firestore

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,18 @@
+import { useDispatch } from "react-redux"
 import { AddOutlined } from "@mui/icons-material"
 import { IconButton, Typography } from "@mui/material"
+import { startNewNote } from "../../store/journal/thunks"
 import { JournalLayout } from "../Layout/JournalLayout"
 import { NoteView } from "../views/NoteView"
 import { NothingSelectedView } from "../views/NothingSelectedView"
 
 export const JournalPage = () => {
+
+  const dispatch = useDispatch()
+
+  const onClickNewNote = () => {
+    dispatch( startNewNote() )
+  }
   return (
     <JournalLayout>
       {/* <Typography>Texto text mucho texto</Typography> */}
@@ -21,6 +29,7 @@ export const JournalPage = () => {
           right:50,
           bottom:50
         }}
+        onClick={onClickNewNote}
       >
         <AddOutlined sx={{ fontSize:30 }}/>
       </IconButton>
