368c8dc57ec2121a336b7e1f7b2a9c0e17f0360a
Eduardo Samaniego <samaniegomsf@gmail.com>
2022-08-09 00:52:19 -0500

    Refactorizamos el sidebar y mostramos las notas en el

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -1,11 +1,12 @@
 import { TurnedInNot } from "@mui/icons-material"
 import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material"
 import { useSelector } from 'react-redux'
+import { SideBarItem } from "./SideBarItem"
 
 export const SideBar = ({ drawerWidth }) => {
   const {displayName} = useSelector(state => state.auth)
   const { notes } = useSelector( state => state.journal)
-  console.log(notes)
+  //console.log(notes)
   return (
     <Box
       component='nav'
@@ -31,17 +32,7 @@ export const SideBar = ({ drawerWidth }) => {
         <List>
           {
             notes.map( note =>(
-              <ListItem key={note.id} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    <TurnedInNot />
-                  </ListItemIcon>
-                  <Grid container sx={{display:'block'}}>
-                    <ListItemText primary={note.title} />
-                    <ListItemText secondary={note.id}/>
-                  </Grid>
-                </ListItemButton>
-              </ListItem>
+              <SideBarItem  key={note.id} {...note} />
             ))
           }
         </List>
