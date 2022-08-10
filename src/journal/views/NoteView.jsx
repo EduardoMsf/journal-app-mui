70c97702ddfa736dc98282f9ff092e4a0df3c843
Eduardo Samaniego <samaniegomsf@gmail.com>
2022-08-10 00:09:59 -0500

    Actualizamos la nota en firestore

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,14 +1,16 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { SaveOutlined } from "@mui/icons-material"
 import { Button, Grid, TextField, Typography } from "@mui/material"
 import { ImageGallery } from "../components"
 import { useForm } from '../../hooks/useForm'
-import { useMemo } from 'react'
+import { useEffect, useMemo } from 'react'
+import { setActiveNote, startSaveNote } from '../../store/journal'
 
 
 export const NoteView = () => {
 
   const { active: note } = useSelector(state => state.journal)
+  const dispatch = useDispatch()
 
   const { title, body, date, onInputChange, formState } = useForm( note )
 
@@ -16,6 +18,15 @@ export const NoteView = () => {
     const newDate = new Date(date)
     return newDate.toUTCString()
   }, [date])
+
+  useEffect(() => {
+    dispatch(setActiveNote(formState))
+  }, [formState])
+
+  const onSaveNote = () =>{
+    dispatch(startSaveNote())
+  }
+  
   
   return (
     <Grid
@@ -30,7 +41,7 @@ export const NoteView = () => {
         <Typography fontSize={39} fontWeight='light'>{dateString}</Typography>
       </Grid>
       <Grid item >
-        <Button color='primary' sx={{ padding:2 }}>
+        <Button onClick={onSaveNote} color='primary' sx={{ padding:2 }}>
           <SaveOutlined sx={{ fontSize:30, mr:1 }} />
           Guardar
         </Button>
--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -2,7 +2,7 @@
 import { collection, doc, setDoc } from "firebase/firestore/lite"
 import { FirebaseDB } from "../../firebase/config"
 import { loadNotes } from "../../helpers/"
-import { addNewEmptyNote, setActiveNote, savingNewNote, setNotes } from "./"
+import { addNewEmptyNote, setActiveNote, savingNewNote, setNotes, setSaving } from "./"
 
 export const startNewNote = () => {
   return async( dispatch, getState ) => {
@@ -35,5 +35,17 @@ export const startLoadingNotes = () => {
     const notes = await loadNotes(uid)
 
     dispatch( setNotes(notes) )
+  }
+}
+
+export const startSaveNote = () => {
+  return async( dispatch, getState ) => {
+    dispatch( setSaving())
+    const { uid } = getState().auth
+    const { active:note } = getState().journal
+    const noteToFireStore = { ...note }
+    delete noteToFireStore.id
+    const docRef = doc(FirebaseDB, `${uid}/journal/notes/${note.id}`)
+    await setDoc( docRef, noteToFireStore, { merge: true } )
   }
 }
