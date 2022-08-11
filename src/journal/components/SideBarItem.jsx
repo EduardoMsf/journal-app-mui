14007d593ba0de921132d5ae0597889ecc09af0c
Eduardo Samaniego <samaniegomsf@gmail.com>
2022-08-11 00:32:31 -0500

    Seleccionar archivos desde react[ui]

--- src/journal/components/SideBarItem.jsx
+++ src/journal/components/SideBarItem.jsx
@@ -1,5 +1,5 @@
 import { useDispatch } from 'react-redux'
-import { TurnedInNot } from '@mui/icons-material'
+import { TurnedIn, TurnedInNot } from '@mui/icons-material'
 import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
 import { useMemo } from 'react'
 import { setActiveNote } from '../../store/journal'
@@ -13,20 +13,26 @@ export const SideBarItem = ({ title = '', body, id, date, imageUrls = [] }) => {
   }
 
   const newTitle = useMemo(() => { 
-    return title.length > 17 
-      ? title.substring(0,17) + '...'
+    return title.length > 15 
+      ? title.substring(0,15) + '...'
       : title
   },[title])
+
+  const newBody = useMemo(() => { 
+    return body.length > 17 
+      ? body.substring(0,17) + '...'
+      : body
+  },[body])
   
   return (
     <ListItem disablePadding>
       <ListItemButton onClick={onClickNote}>
         <ListItemIcon>
-          <TurnedInNot />
+          <TurnedIn color='error' />
         </ListItemIcon>
         <Grid container sx={{display:'block'}}>
           <ListItemText primary={newTitle} />
-          <ListItemText secondary={body}/>
+          <ListItemText secondary={newBody}/>
         </Grid>
       </ListItemButton>
     </ListItem>
--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,9 +1,9 @@
 import { useDispatch, useSelector } from 'react-redux'
-import { SaveOutlined } from "@mui/icons-material"
-import { Button, Grid, TextField, Typography } from "@mui/material"
+import { SaveOutlined, UploadOutlined } from "@mui/icons-material"
+import { Button, Grid, IconButton, TextField, Typography } from "@mui/material"
 import { ImageGallery } from "../components"
 import { useForm } from '../../hooks/useForm'
-import { useEffect, useMemo } from 'react'
+import { useEffect, useMemo, useRef } from 'react'
 import { setActiveNote, startSaveNote } from '../../store/journal'
 import Swal from 'sweetalert2'
 import 'sweetalert2/dist/sweetalert2.css'
@@ -32,9 +32,17 @@ export const NoteView = () => {
   }, [messageSaved])
   
 
-  const onSaveNote = () =>{
+  const onSaveNote = () => {
     dispatch(startSaveNote())
   }
+
+  const onFileInputChange = ({ target }) => {
+    if( target.files === 0 ) return
+    console.log('subiendo archivos')
+    // dispatch( startUploadingFiles( target.files ))
+  }
+
+  const fileInputRef = useRef()
   
   
   return (
@@ -50,6 +58,20 @@ export const NoteView = () => {
         <Typography fontSize={39} fontWeight='light'>{dateString}</Typography>
       </Grid>
       <Grid item >
+        <input
+          type='file'
+          multiple
+          ref={ fileInputRef }
+          onChange={onFileInputChange}
+          style={{ display:'none'}}
+        />
+        <IconButton
+          color='primary'
+          disabled={isSaving}
+          onClick={ () => fileInputRef.current.click()}
+        >
+          <UploadOutlined />
+        </IconButton>
         <Button disabled={isSaving} onClick={onSaveNote} color='primary' sx={{ padding:2 }}>
           <SaveOutlined sx={{ fontSize:30, mr:1 }} />
           Guardar
