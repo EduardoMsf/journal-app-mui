90ca399498d7360e4074b351cf3deec0f7c9598b
Eduardo Samaniego <samaniegomsf@gmail.com>
2023-04-18 18:21:28 -0600

    Cambios al nombrar un componente

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,9 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 import { AuthRoutes } from "../auth/routes/AuthRoutes"
 import { JournalRoutes } from "../journal/routes/JournalRoutes"
-import { CheckingAuth } from "../ui/components/checkingAuth"
+// import { CheckingAuth } from "../ui/components/checkingAuth"
 import { useCheckAuth } from "../hooks"
+import { CheckingAuth } from "../ui/components/CheckingAuth"
 
 export const AppRouter = () => {
   
